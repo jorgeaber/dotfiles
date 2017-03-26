54346db6528ce99671f0d21c6f28b18aa6ac0df5
jorgeaber <jorgeaber@gmail.com>
2017-03-26 00:02:27 -0300

    General update

--- urxvt/install.sh
+++ urxvt/install.sh
@@ -5,9 +5,10 @@ echo -e "\n\nInstalling urxvt and related packages..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 USER_HOME=$(eval echo ~${SUDO_USER})
 
-apt-get -qq install rxvt-unicode tmux fonts-inconsolata zsh wmctrl -y
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+sudo apt-get -qq install rxvt-unicode tmux fonts-inconsolata zsh wmctrl -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 
+mkdir -p ${USER_HOME}/.oh-my-zsh/themes
 cp ${DIR}/mortalscumbag.zsh-theme ${USER_HOME}/.oh-my-zsh/themes
 cp ${DIR}/.* ${USER_HOME}
--- vim/install.sh
+++ vim/install.sh
@@ -9,7 +9,8 @@ apt-get -qq install vim-gnome -y
 git clone https://github.com/VundleVim/Vundle.vim.git ${USER_HOME}/.vim/bundle/Vundle.vim
 
 mkdir -p ${USER_HOME}/.vim/colors 
-curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim > ${USER_HOME}/.vim/colors
+curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim > ${USER_HOME}/.vim/colors/solarized.vim
 
-cp ${DIR}/.vimrc ${USER_HOME}/.vimrc /root/.vimrc
+cp ${DIR}/.vimrc ${USER_HOME}/.vimrc 
+cp ${DIR}/.vimrc /root/.vimrc
 cp -r ${USER_HOME}/.vim /root/.vim 
